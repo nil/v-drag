4a5068890148a8c408deb5ec4c4f61ee49390acd
Nil Vila <nilvilamar@gmail.com>
2018-08-28 09:40:19 +0200

    Linted js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@
  * by Nil Vila
  */
 
-let data = {
-  axis: "all",
+const data = {
+  axis: 'all',
 
   isStyleAdded: false,
 
   transform: {
     declared: false,
-    string: ""
+    string: ''
   },
 
   initialX: 0,
@@ -18,41 +18,43 @@ let data = {
 
   cursorInitialX: 0,
   cursorInitialY: 0
-}
+};
 
-let elements = {
+const elements = {
   grab: null,
   move: null
-}
+};
 
-let eventClass = {
-  initial: "drag-draggable",
-  hasHandle: "drag-uses-handle",
-  handle: "drag-handle",
-  down: "drag-down",
-  move: "drag-move"
-}
-
-function isObject(val, ifTrue, ifFalse) {
-  if (val instanceof Object) {
-    return ifTrue
-  } else {
-    return ifFalse
-  }
-}
+const eventClass = {
+  initial: 'drag-draggable',
+  hasHandle: 'drag-uses-handle',
+  handle: 'drag-handle',
+  down: 'drag-down',
+  move: 'drag-move'
+};
 
 function returnPositionString(a, b) {
-  return `matrix(${data.transform.string}, ${a}, ${b})`
+  return `matrix(${data.transform.string}, ${a}, ${b})`;
+}
+
+function getMatrixSection(str) {
+  const list = []; let i;
+  for (i = 0; i < str.length; i += 1) {
+    if (str[i] === ',') {
+      list.push(i);
+    }
+  }
+  return list;
 }
 
 function getPosition(str, el, dir) {
-  let list = getMatrixSection(str)
-  let pos = parseInt(window.getComputedStyle(el)[dir].slice(0, -2));
+  const list = getMatrixSection(str);
+  let pos = parseInt(window.getComputedStyle(el)[dir].slice(0, -2), 10);
 
-  if (dir === "left" && str !== "none") {
-    pos += parseInt(str.slice(list[3] + 2, list[4]));
-  } else if (dir === "top" && str !== "none") {
-    pos += parseInt(str.slice(list[4] + 2, -1));
+  if (dir === 'left' && str !== 'none') {
+    pos += parseInt(str.slice(list[3] + 2, list[4]), 10);
+  } else if (dir === 'top' && str !== 'none') {
+    pos += parseInt(str.slice(list[4] + 2, -1), 10);
   }
 
   return pos;
@@ -62,23 +64,50 @@ function getMousePosition(e) {
   return {
     x: (e.pageX || e.touches[0].pageX) - data.cursorInitialX,
     y: (e.pageY || e.touches[0].pageY) - data.cursorInitialY
-  }
+  };
 }
 
-function getMatrixSection(str) {
-  let list = [], i;
-  for(i = 0; i < str.length; i++) {
-    if (str[i] === ",") {
-      list.push(i);
-    }
+/* --- While dragging ---------- */
+const updatePosition = {
+  addClass() {
+    elements.move.classList.add(eventClass.move);
+    updatePosition.addClass = function () {};
+  },
+  x(e) {
+    const pos = getMousePosition(e);
+
+    updatePosition.addClass();
+    data.relativeX = pos.x;
+    elements.move.style.transform = returnPositionString(data.initialX + pos.x, data.initialY);
+  },
+  y(e) {
+    const pos = getMousePosition(e);
+
+    updatePosition.addClass();
+    data.relativeY = pos.y;
+    elements.move.style.transform = returnPositionString(data.initialX, data.initialY + pos.y);
+  },
+  all(e) {
+    const pos = getMousePosition(e);
+
+    updatePosition.addClass();
+    data.relativeX = pos.x;
+    data.relativeY = pos.y;
+
+    elements.move.style.transform = returnPositionString(
+      data.initialX + pos.x,
+      data.initialY + pos.y
+    );
   }
-  return list;
-}
+};
 
 /* --- Start dragging ---------- */
 function dragDown(axis, grabElement, moveElement, e) {
-  elements.grab = grabElement;
-  elements.move = moveElement;
+  const grabEl = grabElement;
+  const moveEl = moveElement;
+
+  elements.grab = grabEl;
+  elements.move = moveEl;
 
   data.axis = axis;
 
@@ -88,105 +117,73 @@ function dragDown(axis, grabElement, moveElement, e) {
   data.relativeX = 0;
   data.relativeY = 0;
 
-  let matrix = window.getComputedStyle(moveElement).transform;
+  const matrix = window.getComputedStyle(moveElement).transform;
 
-  if (matrix == "none") {
+  if (matrix === 'none') {
     data.transform.declared = false;
-    data.transform.string = "1, 0, 0, 1";
+    data.transform.string = '1, 0, 0, 1';
   } else {
     data.transform.declared = true;
     data.transform.string = matrix.slice(7, getMatrixSection(matrix)[3]);
   }
 
-  data.initialX = getPosition(matrix, moveElement, "left");
-  data.initialY = getPosition(matrix, moveElement, "top");
+  data.initialX = getPosition(matrix, moveEl, 'left');
+  data.initialY = getPosition(matrix, moveEl, 'top');
 
-  moveElement.style.transform = returnPositionString(data.initialX, data.initialY);
-  moveElement.style.left = 0;
-  moveElement.style.top = 0;
+  moveEl.style.transform = returnPositionString(data.initialX, data.initialY);
+  moveEl.style.left = 0;
+  moveEl.style.top = 0;
 
-  grabElement.classList.add(eventClass.down);
+  grabEl.classList.add(eventClass.down);
 
-  document.addEventListener("mousemove", updatePosition[axis]);
-  document.addEventListener("touchmove", updatePosition[axis]);
-}
-
-/* --- Dragging ---------- */
-const updatePosition = {
-  addClass: function() {
-    elements.move.classList.add(eventClass.move);
-    updatePosition.addClass = function() {}
-  },
-  x: function(e) {
-    let pos = getMousePosition(e);
-
-    updatePosition.addClass();
-    data.relativeX = pos.x;
-    elements.move.style.transform = returnPositionString(data.initialX + pos.x, data.initialY);
-  },
-  y: function(e) {
-    let pos = getMousePosition(e);
-
-    updatePosition.addClass();
-    data.relativeY = pos.y;
-    elements.move.style.transform = returnPositionString(data.initialX, data.initialY + pos.y);
-  },
-  all: function(e) {
-    let pos = getMousePosition(e);
-
-    updatePosition.addClass();
-    data.relativeX = pos.x;
-    data.relativeY = pos.y;
-    elements.move.style.transform = returnPositionString(data.initialX + pos.x, data.initialY + pos.y);
-  }
+  document.addEventListener('mousemove', updatePosition[axis]);
+  document.addEventListener('touchmove', updatePosition[axis]);
 }
 
 /* --- End dragging ---------- */
 function dragUp() {
   if (elements.move) {
-    elements.move.style.transform = data.transform.declared ? returnPositionString(0, 0) : "none";
-    elements.move.style.left = data.initialX + data.relativeX + "px";
-    elements.move.style.top = data.initialY + data.relativeY + "px";
+    elements.move.style.transform = data.transform.declared ? returnPositionString(0, 0) : 'none';
+    elements.move.style.left = `${data.initialX + data.relativeX}px`;
+    elements.move.style.top = `${data.initialY + data.relativeY}px`;
 
-    updatePosition.addClass = function() {
+    updatePosition.addClass = function () {
       elements.move.classList.add(eventClass.move);
-      updatePosition.addClass = function() {}
+      updatePosition.addClass = function () {};
     };
 
     elements.grab.classList.remove(eventClass.down);
     elements.move.classList.remove(eventClass.move);
 
-    document.removeEventListener("mousemove", updatePosition[data.axis]);
-    document.removeEventListener("touchmove", updatePosition[data.axis]);
+    document.removeEventListener('mousemove', updatePosition[data.axis]);
+    document.removeEventListener('touchmove', updatePosition[data.axis]);
   }
 }
 
 function createDraggable(el, binding) {
-  let val = binding.value;
-  let axis, handle, grabElement, moveElement;
+  const val = binding.value;
+  let axis; let handle; let grabElement; let moveElement;
 
   /* Creates stylesheet with basic styling (position, z-index and cursors) */
   if (!data.isStyleAdded) {
     data.isStyleAdded = true;
 
-    let styleElement = document.createElement("style");
+    const styleElement = document.createElement('style');
     styleElement.innerHTML = `.${eventClass.initial}{position:relative;}.${eventClass.initial}:not(.${eventClass.hasHandle}),.${eventClass.handle}{cursor:move;cursor:grab;cursor:-webkit-grab;cursor:-moz-grab;}.${eventClass.handle}.${eventClass.down},.${eventClass.initial}:not(.${eventClass.hasHandle}).${eventClass.down}{z-index:999;cursor:grabbing;cursor:-webkit-grabbing;cursor:-moz-grabbing;}`;
     document.body.appendChild(styleElement);
   }
 
   if (val instanceof Object) {
-    axis = val.axis;
-    handle = val.handle;
+    [axis, handle] = [val.axis, val.handle];
   } else {
-    axis = binding.arg;
-    handle = val;
+    [axis, handle] = [binding.arg, val];
   }
 
-  if (axis != "x" && axis != "y") {
-    axis = "all";
+  if (axis !== 'x' && axis !== 'y') {
+    axis = 'all';
   }
 
-  let valueElement = document.getElementById(handle);
+  const valueElement = document.getElementById(handle);
 
   if (val && !valueElement && val.handle) {
     console.error(`Element with id “${val.handle || val}” doesn’t exist`);
@@ -209,39 +206,41 @@ function createDraggable(el, binding) {
   }
 
   /* End dragging */
-  document.addEventListener("mouseup", dragUp);
-  document.addEventListener("touchend", dragUp);
+  document.addEventListener('mouseup', dragUp);
+  document.addEventListener('touchend', dragUp);
 }
 
 const vDrag = {
   install(Vue, options) {
     if (options) {
-      let classes = options.eventClass;
-      for (let key in eventClass) {
+      const classes = options.eventClass;
+      eventClass.forEach((key) => {
         if (classes[key]) {
           eventClass[key] = classes[key];
         }
-      }
+      });
     }
 
-    Vue.directive("drag", {
-      inserted: function(el, binding) {
+    Vue.directive('drag', {
+      inserted(el, binding) {
         createDraggable(el, binding);
       },
-      update: function(el, binding) {
-        let oldHandle = document.getElementById(binding.oldValue) || document.getElementById(binding.oldValue.handle);
+      update(el, binding) {
+        if (binding.oldValue) {
+          const oldHandle = document.getElementById(binding.oldValue)
+            || document.getElementById(binding.oldValue.handle);
 
-        if (oldHandle) {
-          console.log(oldHandle);
-          oldHandle.onmousedown = null;
-          oldHandle.ontouchstart = null;
-          oldHandle.classList.remove(eventClass.handle)
+          if (oldHandle) {
+            oldHandle.onmousedown = null;
+            oldHandle.ontouchstart = null;
+            oldHandle.classList.remove(eventClass.handle);
+          }
         }
         createDraggable(el, binding);
       }
-    })
+    });
   }
-}
+};
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(vDrag);
