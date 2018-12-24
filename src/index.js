f8c9170e2edcf904e8abf903dace3032bfcc4fd1
Nil Vila <nilvilamar@gmail.com>
2018-12-24 15:34:55 +0100

    :bug: Added CSS classes to elements

--- src/index.js
+++ src/index.js
@@ -6,10 +6,22 @@
 
 import dragSetup from './drag/dragSetup';
 
+const eventClass = {
+  initial: 'drag-draggable',
+  usesHandle: 'drag-uses-handle',
+  handle: 'drag-handle',
+  down: 'drag-down',
+  move: 'drag-move'
+};
+
 export default {
   install(Vue) {
-    Vue.directive('drag', {
+    // Create stylesheet with basic styling (position, z-index and cursors)
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = `.${eventClass.initial}{position:relative;}.${eventClass.initial}:not(.${eventClass.usesHandle}),.${eventClass.handle}{cursor:move;cursor:grab;cursor:-webkit-grab;cursor:-moz-grab;}.${eventClass.handle}.${eventClass.down},.${eventClass.initial}:not(.${eventClass.usesHandle}).${eventClass.down}{z-index:999;cursor:grabbing;cursor:-webkit-grabbing;cursor:-moz-grabbing;}`;
+    document.body.appendChild(styleElement);
 
+    Vue.directive('drag', {
       // Add draggable configuration to element for the first time
       inserted(el, binding, vnode) {
         dragSetup(el, binding, vnode);
