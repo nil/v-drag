f89faee640f0761690a76239bdb520c7f0a75832
Nil Vila <nilvilamar@gmail.com>
2019-02-09 14:45:49 +0100

    :bug: Removed transitions on draggable elements

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ export default {
 
     // Create stylesheet with basic styling (position, z-index and cursors)
     const styleElement = document.createElement('style');
-    styleElement.innerHTML = `.${window.data.class.initial}{position:relative;}.${window.data.class.initial}:not(.${window.data.class.usesHandle}),.${window.data.class.handle}{cursor:move;cursor:grab;cursor:-webkit-grab;cursor:-moz-grab;}.${window.data.class.handle}.${window.data.class.down},.${window.data.class.initial}:not(.${window.data.class.usesHandle}).${window.data.class.down}{z-index:999;cursor:grabbing;cursor:-webkit-grabbing;cursor:-moz-grabbing;}`;
+    styleElement.innerHTML = `.${window.data.class.initial}{position:relative;transition:none;}.${window.data.class.initial}:not(.${window.data.class.usesHandle}),.${window.data.class.handle}{cursor:move;cursor:grab;cursor:-webkit-grab;cursor:-moz-grab;}.${window.data.class.handle}.${window.data.class.down},.${window.data.class.initial}:not(.${window.data.class.usesHandle}).${window.data.class.down}{z-index:999;cursor:grabbing;cursor:-webkit-grabbing;cursor:-moz-grabbing;}`;
     document.body.appendChild(styleElement);
 
     // Register 'v-drag' directive
