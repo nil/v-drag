d4dcf314451f2860e71967e680ae1d63a0f0e6da
Nil Vila <nilvilamar@gmail.com>
2019-03-02 16:21:09 +0100

    Improved LocalePicker

--- docs/.vuepress/theme/components/LocalePicker.vue
+++ docs/.vuepress/theme/components/LocalePicker.vue
@@ -1,8 +1,18 @@
 <template>
-  <div class="nav-links">
-    <IconLocale />
-    <div class="nav-item">
-      <DropdownLink :item="localeList" />
+  <div class="locale-picker">
+    <button class="locale-picker--button"
+      @click="openDropdown(false)"
+      @mouseenter="openDropdown(500)"
+      :disabled="isDropdownOpen"
+      ref="localePickerButton">
+      <IconLocale />
+    </button>
+
+    <div class="locale-picker--dropdown"
+      :class="{ active: isDropdownOpen }"
+      @mouseleave="closeDropdown(300)"
+      ref="localePickerDropdown">
+      <NavLink v-for="locale in localeList" :item="locale" />
     </div>
   </div>
 </template>
@@ -10,6 +20,7 @@
 <script>
 
 import DropdownLink from '@theme/components/DropdownLink.vue'
+import NavLink from '@theme/components/NavLink.vue'
 import IconLocale from './icons/IconLocale.vue';
 
 export default {
@@ -17,50 +28,73 @@ export default {
 
   components: {
     DropdownLink,
+    NavLink,
     IconLocale
+  },
+
+  data() {
+    return {
+      isDropdownOpen: false
+    }
   },
 
   computed: {
     localeList() {
-      const { locales } = this.$site
-      const currentLink = this.$page.path
-      const routes = this.$router.options.routes
-      const themeLocales = this.$site.themeConfig.locales || {}
-      const languageDropdown = {
-        text: this.$themeLocaleConfig.selectText || 'Languages',
-        items: Object.keys(locales).map(path => {
-          const locale = locales[path]
-          const localeName = this.$site.locales[path].name;
-          let link
-          // Stay on the current page
-          if (locale.lang === this.$lang) {
-            link = currentLink
-          } else {
-            // Try to stay on the same page
-            link = currentLink.replace(this.$localeConfig.path, path)
-            // fallback to homepage
-            if (!routes.some(route => route.path === link)) {
-              link = path
-            }
+      const { locales } = this.$site;
+      const currentLink = this.$page.path;
+      const routes = this.$router.options.routes;
+
+      // List of locales and links
+      return Object.keys(locales).map(path => {
+        const localeInfo = locales[path];
+        const localeName = this.$site.locales[path].name;
+        let localeLink;
+
+        // Stay on the same page
+        if (localeInfo.lang === this.$lang) {
+          localeLink = currentLink;
+        } else {
+          // Change the language of the page
+          localeLink = currentLink.replace(this.$localeConfig.path, path);
+
+          // Fallback to homepage
+          if (!routes.some(route => route.path === localeLink)) {
+            localeLink = path;
           }
-          return {
-            text: localeName,
-            link }
-        })
-      }
+        }
 
-      console.log(languageDropdown);
+        // Create object with text an link
+        return {
+          text: localeName,
+          link: localeLink
+        }
+      });
+    }
+  },
 
-      return languageDropdown;
-      // return { text: 'Locale', type: 'links', items: [
-      //   { text: 'Español', link: '/es/', type: 'link'},
-      //   { text: 'English', link: '/', type: 'link'},
-      //   { text: 'Français', link: '/fr/', type: 'link'}
-      // ]}
+  methods: {
+    openDropdown(time) {
+      const timer = setTimeout(() => {
+        this.isDropdownOpen = true;
+      }, time);
+
+      this.$refs.localePickerButton.addEventListener('mouseleave', function () {
+        clearTimeout(timer);
+      });
+    },
+
+    closeDropdown(time) {
+      const timer = setTimeout(() => {
+        this.isDropdownOpen = false;
+      }, time);
+
+      this.$refs.localePickerDropdown.addEventListener('mouseenter', function () {
+        clearTimeout(timer);
+      });
     }
   }
-
 }
+
 </script>
 
 <style>
@@ -68,4 +102,5 @@ svg.icon-locale {
   width: 24px;
   fill: red;
 }
+.router-link-active { color: red; }
 </style>
