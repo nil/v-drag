c56ee70631be2bc848555d3bef63c8b1d6f6cab5
Nil Vila <nilvilamar@gmail.com>
2019-03-14 16:14:42 +0100

    Updated module format to umd, hopefully fixing #4

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,6 @@
 import { terser } from 'rollup-plugin-terser';
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
 import info from './package.json';
 
 const bannerText = `/*!
@@ -11,10 +13,12 @@ export default {
   output: {
     file: 'dist/main.js',
     name: 'optionsConfig',
-    format: 'esm',
+    format: 'umd',
     banner: bannerText
   },
   plugins: [
+    nodeResolve(),
+    commonjs(),
     terser({
       output: {
         comments: /^!/
