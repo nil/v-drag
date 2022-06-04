73b39520875a8e72599e76c5e03fafaf7b941f1c
Nil Vila <nilvilamar@gmail.com>
2022-06-04 09:34:07 +0200

    Add delay

--- src/utils/updateMousePosition.js
+++ src/utils/updateMousePosition.js
@@ -5,20 +5,23 @@ export default function (e) {
   window.data.mouseX = (e.pageX || e.touches[0].pageX) - window.data.initialX;
   window.data.mouseY = (e.pageY || e.touches[0].pageY) - window.data.initialY;
 
-  // Scroll page if dragging over the edges
-  if ((e.clientY || e.touches[0].clientY) > window.innerHeight * 0.8) {
-    document.documentElement.scrollTop += 10;
-  }
+  // Scroll page if dragging over the edges,
+  // but only if mouse is static for some time
+  window.setTimeout(() => {
+    if ((e.clientY || e.touches[0].clientY) > window.innerHeight * 0.8) {
+      document.documentElement.scrollTop += 10;
+    }
 
-  if ((e.clientY || e.touches[0].clientY) < window.innerHeight * 0.2) {
-    document.documentElement.scrollTop -= 10;
-  }
+    if ((e.clientY || e.touches[0].clientY) < window.innerHeight * 0.2) {
+      document.documentElement.scrollTop -= 10;
+    }
 
-  if ((e.clientX || e.touches[0].clientX) > window.innerWidth * 0.8) {
-    document.documentElement.scrollLeft += 10;
-  }
+    if ((e.clientX || e.touches[0].clientX) > window.innerWidth * 0.8) {
+      document.documentElement.scrollLeft += 10;
+    }
 
-  if ((e.clientX || e.touches[0].clientX) < window.innerWidth * 0.2) {
-    document.documentElement.scrollLeft -= 10;
-  }
+    if ((e.clientX || e.touches[0].clientX) < window.innerWidth * 0.2) {
+      document.documentElement.scrollLeft -= 10;
+    }
+  }, 100);
 }
