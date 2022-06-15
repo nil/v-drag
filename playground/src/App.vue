19fe0f1336fd1671024a1a77462a27da302fc4d6
Nil Vila <nilvilamar@gmail.com>
2022-06-15 15:09:24 +0200

    App setup

---
+++ playground/src/App.vue
@@ -0,0 +1,27 @@
+/* eslint-disable css/unknown-at-ru */
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue';
+
+import './styles/global.css';
+</script>
+
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Hello Vue 3 + Vite" />
+  </div>
+</template>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ playground/src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+// Create a new store instance.
+const store = createStore({
+  state() {
+    return {
+      count: 0,
+    };
+  },
+
+  mutations: {
+    increment(state, number) {
+      state.count += number;
+    },
+  },
+});
+
+export default store;
