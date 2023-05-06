16c076f94ee6d4ffc40fa74fe399d175ce201353
Nil Vila <nilvilamar@gmail.com>
2023-05-06 11:21:09 +0200

    Removed scrolling beyond edges when dragging

--- src/utils/updateMousePosition.js
+++ src/utils/updateMousePosition.js
@@ -7,21 +7,22 @@ export default function (e) {
 
   // Scroll page if dragging over the edges,
   // but only if mouse is static for some time
-  window.setTimeout(() => {
-    if ((e.clientY) > window.innerHeight * 0.8) {
-      document.documentElement.scrollTop += 10;
-    }
+  // TODO: Improve this, make it more fluid and natural
+  // window.setTimeout(() => {
+  //   if ((e.clientY) > window.innerHeight * 0.8) {
+  //     document.documentElement.scrollTop += 10;
+  //   }
 
-    if ((e.clientY) < window.innerHeight * 0.2) {
-      document.documentElement.scrollTop -= 10;
-    }
+  //   if ((e.clientY) < window.innerHeight * 0.2) {
+  //     document.documentElement.scrollTop -= 10;
+  //   }
 
-    if ((e.clientX) > window.innerWidth * 0.8) {
-      document.documentElement.scrollLeft += 10;
-    }
+  //   if ((e.clientX) > window.innerWidth * 0.8) {
+  //     document.documentElement.scrollLeft += 10;
+  //   }
 
-    if ((e.clientX) < window.innerWidth * 0.2) {
-      document.documentElement.scrollLeft -= 10;
-    }
-  }, 100);
+  //   if ((e.clientX) < window.innerWidth * 0.2) {
+  //     document.documentElement.scrollLeft -= 10;
+  //   }
+  // }, 100);
 }
